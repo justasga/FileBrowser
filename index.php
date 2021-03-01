b6d5f7509c9aacd45d2833d478e7c6ab3c379c32
justasga <74532971+justasga@users.noreply.github.com>
2021-03-01 22:54:45 +0200

    3/1

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    require_once('inc/autoload.php');
+    $path = "ROOT_PATH";
+    $rel = "DS";
+    $up = null;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div id= "wrapper">
+
+    <?php require_once('mod/structure.php'); ?>
+
+</div>
+
+</body>
+</html>
